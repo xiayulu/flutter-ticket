938dfa8ebd6808eede2ba279b6d381840ac80390
xiayulu <harolddfinchh@gmail.com>
2023-06-29 17:17:26 +0800

    feat: more rows

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:ticket/utils/app_styles.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,6 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(children: [
+            const Gap(10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -27,6 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       "Good Morning",
                       style: Styles.headLineStyle3,
                     ),
+                    const Gap(5),
                     Text(
                       "Book Tickets",
                       style: Styles.headLineStyle1,
@@ -43,9 +47,46 @@ class _HomeScreenState extends State<HomeScreen> {
                           image: AssetImage("assets/images/img_1.png"))),
                 )
               ],
-            )
+            ),
+            const Gap(25),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xFFf4f6fd)),
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  const Icon(FluentSystemIcons.ic_fluent_search_regular),
+                  Text(
+                    "Search",
+                    style: Styles.headLineStyle4,
+                  )
+                ],
+              ),
+            ),
+            const Gap(40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Upcomming Flights",
+                  style: Styles.headLineStyle2,
+                ),
+                InkWell(
+                  onTap: () {
+                    print("view More");
+                  },
+                  child: Text(
+                    "View All",
+                    style:
+                        Styles.textStyle.copyWith(color: Styles.primaryColor),
+                  ),
+                )
+              ],
+            ),
+            const Gap(40),
           ]),
-        )
+        ),
       ]),
     );
   }
