aedf522e07e60ec021e86a771502c466c77d220a
xiayulu <harolddfinchh@gmail.com>
2023-07-06 10:52:01 +0800

    add get

--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AppLayout {
   static getSize(BuildContext context) {
     return MediaQuery.of(context).size;
+  }
+
+  static getScreenHeight() {
+    return Get.height;
+  }
+
+  static getScreenWidth() {
+    return Get.width;
   }
 }
