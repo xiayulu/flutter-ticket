da00acaf30c9a9810b526cedd264213148c57e92
xiayulu <harolddfinchh@gmail.com>
2023-07-07 18:54:54 +0800

    add dot

--- lib/widgets/pin.dart
+++ lib/widgets/pin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket/utils/app_styles.dart';
 
 class TiPin extends StatelessWidget {
   const TiPin({super.key});
@@ -6,21 +7,13 @@ class TiPin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(5),
-      decoration: const BoxDecoration(
-        color: Colors.black,
-        shape: BoxShape.circle,
-      ),
-      child: SizedBox(
-        width: 20,
-        height: 20,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(width: 5, color: Colors.white),
-              color: Colors.black),
-        ),
-      ),
-    );
+        padding: const EdgeInsets.all(3),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Styles.textColor, width: 2)),
+        child: CircleAvatar(
+          maxRadius: 4,
+          backgroundColor: Styles.textColor,
+        ));
   }
 }
